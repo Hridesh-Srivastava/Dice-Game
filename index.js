e4c3ad2364c48da8cf3dd96a17f9fe6df3d69039
Hridesh <hrideshsrivastavaa@outlook.com>
2024-07-24 23:19:53 +0530

    fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomNumber1=Math.random()*6;
 randomNumber1=Math.floor(randomNumber1)+1;
 //var randomNumber1=Math.floor(Math.random()*6)+1;
 var diceGame="dice" + randomNumber1 +".png";
-var dicesses="./images/"+diceGame;
+var dicesses="./"+diceGame;
 var dicers=document.querySelectorAll("img")[0]; //array 0 should be mentioned coz we have to specify which no. of img we are changing so for 1st img we have taken 0 in array (as array reads from zero).
 
 dicers.setAttribute("src",dicesses);
@@ -12,7 +12,7 @@ dicers.setAttribute("src",dicesses);
 var randomNumber2=Math.random()*6;
  var randomNumber2=Math.floor(randomNumber2)+1;
 var dicerrr="dice"+randomNumber2+".png";
-var diceboard="./images/"+dicerrr;
+var diceboard="./"+dicerrr;
 var boardGame=document.querySelectorAll("img")[1]; //array specified tells konsi no. ki img hai
 boardGame.setAttribute("src",diceboard);
 
